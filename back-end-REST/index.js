0af6cbc47020f5ed77c6d3d2a35917b4507cb21b
Matheus Pereira <empregos97@gmail.com>
2023-04-09 13:03:49 -0300

    fix: api built

--- back-end-REST/index.js
+++ back-end-REST/index.js
@@ -1,38 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 const port = 3001;
-const api = require("./data/uiAPI.json");
 
-app.get("/", (req, res) => {
-  res.send("up");
-});
+const apiFilePath = path.join(__dirname, "data", "uiAPI.json");
+const apiReadFile = fs.readFileSync(apiFilePath, "utf-8");
 
-app
-  .route("/api")
-  .get((req, res) => {
-    res.json(api);
-  })
-  .post((req, res) => {
-    const postValues = { ...req.body };
-    api = [...api, ...postValues];
-    res.json(postValues);
-  })
-  .put((req, res) => {
-    let putValues;
-    api = api.map((obj) => {
-      if (obj === req.body) {
-        putValues = { ...obj, ...req.body };
-        return putValues;
-      }
-      return obj;
-    });
-    res.json(putValues);
-  })
-  .delete((req, res) => {
-    const deleteValues = api.find((obj) => obj === +req.body);
-    api = api.filter((obj) => obj !== +req.body);
-    res.json(deleteValues);
-  });
+app.get("/api", (req, res) => apiReadFile && res.json(apiReadFile));
 
 app.listen(port, () => {
   console.log("running");
