6f6456bc0db25bb5161f24058aece1c400296b4c
Matheus Pereira <empregos97@gmail.com>
2023-04-09 12:29:52 -0300

    feature: api for home client-react

--- back-end-REST/index.js
+++ back-end-REST/index.js
@@ -3,27 +3,36 @@ const app = express();
 const port = 3001;
 const api = require("./data/uiAPI.json");
 
-// const aboutTexts = {
-//   Button: "Conecte-se ! ",
-//   moreLink:
-//     "https://www.figma.com/file/6khinzowdFkFD1ikyQNawW/VS-Connect-by-Matheus-Henrique?node-id=1-2&t=NzZOCXGkSUpsEfNK-0",
-//   H1Content: {
-//     firstText: ["Publique", "=", "Nescessidades"],
-//     secondText: ["Encontre", "=", "Desenvolvedores"],
-//     paragraphContent: [
-//       "Se você tem uma idéia, uma empresa , um aplicativo, uma arte ou um negócio...",
-//       "O seu lugar é aqui !",
-//     ],
-//   },
-// };
-
 app.get("/", (req, res) => {
   res.send("up");
 });
 
-app.route("/api").get((req, res) => {
-  res.json(api);
-});
+app
+  .route("/api")
+  .get((req, res) => {
+    res.json(api);
+  })
+  .post((req, res) => {
+    const postValues = { ...req.body };
+    api = [...api, ...postValues];
+    res.json(postValues);
+  })
+  .put((req, res) => {
+    let putValues;
+    api = api.map((obj) => {
+      if (obj === req.body) {
+        putValues = { ...obj, ...req.body };
+        return putValues;
+      }
+      return obj;
+    });
+    res.json(putValues);
+  })
+  .delete((req, res) => {
+    const deleteValues = api.find((obj) => obj === +req.body);
+    api = api.filter((obj) => obj !== +req.body);
+    res.json(deleteValues);
+  });
 
 app.listen(port, () => {
   console.log("running");
