45a37157ebef0161153bff434b06bea183c05b45
Matheus Pereira <empregos97@gmail.com>
2023-04-13 13:38:35 -0300

    BREKING CHANGE:[client-react 7079a4a] feature:i18next-browser-languagedetector

--- client-react/src/features/Test.jsx
+++ client-react/src/features/Test.jsx
@@ -3,13 +3,11 @@ import { initReactI18next, useTranslation } from "react-i18next";
 import enTranslation from "../../locales/en/entranslation.json";
 import ptTranslation from "../../locales/pt/pttranslation.json";
 import LanguageDetector from "i18next-browser-languagedetector";
-
 i18next
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     supportedLngs: ["pt", "en"],
-    lgn: "en" || "pt",
     debug: false,
     fallbackLng: document.querySelector("html").lang,
     resources: {
@@ -19,19 +17,16 @@ i18next
       pt: {
         translation: ptTranslation,
       },
-      detection: {
-        // permite que eu escolha de quais formas eu irei detectar traducoes .
-        order: [
-          "htmlTag",
-          "querystring",
-          "cookie",
-          "localStorage",
-          "sessionStorage",
-          "navigator",
-          "path",
-          "subdomain",
-        ],
-      },
+    },
+    detection: {
+      order: [
+        "htmlTag",
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+      ],
     },
   });
 
