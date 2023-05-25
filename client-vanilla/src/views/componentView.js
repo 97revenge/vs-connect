4573e96e3acaa9f4d78f58091ae839c0e9b2b505
Matheus Henrique <empregos97@gmail.com>
2023-05-25 13:17:14 -0300

    finish: layout complete (1.0.0)

--- client-vanilla/src/views/componentView.js
+++ client-vanilla/src/views/componentView.js
@@ -28,6 +28,7 @@
       const aLinked = routes.map((item) => {
         const a = document.createElement("a");
         a.setAttribute("href", `#${item}`);
+        a.classList.add("banner_botao_dev");
         a.textContent = item;
         return a;
       });
@@ -57,10 +58,30 @@
     },
     section: () => {
       const section = document.createElement("section");
-      section.classList.add("banner");
       const main = document.querySelector("main");
-      main.appendChild(section);
+
+      const sectionBanner = section.cloneNode(true);
+      sectionBanner.classList.add("banner");
+
+      const sectionClient = section.cloneNode(true);
+      sectionClient.classList.add("clientes");
+
+      const div = document.createElement("div");
+      div.classList.add("paralax");
+
+      const sectionDeveloper = section.cloneNode(true);
+      sectionDeveloper.classList.add("desenvolvedores");
+
+      const sectionService = section.cloneNode(true);
+      sectionService.classList.add("servicos");
+
+      main.appendChild(sectionBanner);
+      main.appendChild(sectionClient);
+      main.appendChild(div);
+      main.appendChild(sectionDeveloper);
+      main.appendChild(sectionService);
     },
+
     bannerContent: () => {
       const bannerContent = document.createElement("div");
       bannerContent.classList.add("banner_conteudo");
@@ -113,6 +134,169 @@
       linkClient.setAttribute("class", "botao banner_botao_cliente");
       bannerContent.appendChild(linkClient);
     },
+    clientConection: () => {
+      const section = document.querySelectorAll("section").item(1);
+      const div = document.createElement("div");
+      div.classList.add("conexao");
+
+      const p = document.createElement("p");
+      p.innerHTML = `<strong>Desde 2015 unindo propósitos.<br> Uma nova forma de
+      desenvolver.</strong>`;
+      div.appendChild(p);
+      section.appendChild(div);
+    },
+    clientContent: () => {
+      const img = document.createElement("img");
+
+      const section = document.querySelectorAll("section").item(1);
+
+      const div = document.createElement("div");
+
+      divClientContent = div.cloneNode(false);
+      divClientContent.classList.add("cliente_conteudo");
+
+      divClientText = div.cloneNode(false);
+      divClientText.classList.add("cliente_texto");
+
+      img.setAttribute("src", "../img/clientes.png");
+      divClientContent.appendChild(img);
+
+      const h2 = document.createElement("h2");
+      h2.innerHTML = `Para Clientes 👦🏻👨🏻👵🏻 `;
+
+      const ul = document.createElement("ul");
+
+      const liNode = document.createElement("li");
+
+      const shearch = liNode.cloneNode(true);
+      shearch.innerHTML = `procurar por desenvolvedores 👀`;
+
+      const register = liNode.cloneNode(true);
+      register.innerHTML = `cadastrar servicos 📝`;
+
+      divClientText.appendChild(h2);
+      ul.appendChild(shearch);
+      ul.appendChild(register);
+      divClientText.appendChild(ul);
+      divClientContent.appendChild(divClientText);
+      section.appendChild(divClientContent);
+    },
+    clientButton: () => {
+      const div = document.createElement("div");
+      const clientText = document.querySelectorAll("div").item(5);
+      clientText.appendChild(div);
+      div.setAttribute("class", "botao clientes_botao");
+      div.innerHTML = `<strong>✨ criar conta ✨</strong>`;
+
+      console.log(clientText);
+    },
+
+    devSection: () => {
+      const section = document.querySelectorAll("section").item(2);
+
+      const div = document.querySelector("div").cloneNode(false);
+      div.classList.remove("posicionamento_header");
+
+      const divContent = div.cloneNode(false);
+      divContent.classList.add("dev_conteudo");
+
+      const divInstance = div.cloneNode(false);
+
+      const h2 = document.createElement("h2");
+      h2.innerHTML = `para desenvolvedores 👩🏻‍💻👨🏻‍💻`;
+
+      const ul = document.createElement("ul");
+
+      const li = document.querySelectorAll("li").item(0);
+
+      const services = li.cloneNode(false);
+      services.innerHTML = `Encontrar Servicos 🪓🔫`;
+
+      const hardSkills = li.cloneNode(false);
+      hardSkills.innerHTML = `Divulgar suas HardSkills 🔥👨🏻‍💻`;
+
+      const a = document.createElement("a");
+      a.classList.add("botao");
+      a.innerHTML = `Criar conta `;
+
+      const img = document.createElement("img");
+
+      img.setAttribute("src", "../img/maos_dev.png");
+
+      divInstance.appendChild(h2);
+      ul.appendChild(hardSkills);
+      ul.appendChild(services);
+      divInstance.appendChild(ul);
+      divInstance.appendChild(a);
+      divContent.appendChild(img);
+      divContent.appendChild(divInstance);
+      section.appendChild(divContent);
+    },
+
+    serviceSection: () => {
+      const section = document.querySelectorAll("section").item(3);
+      const div = document.createElement("div");
+
+      const divService = div.cloneNode(false);
+      divService.classList.add("servicos_cards");
+
+      const cardLeft = div.cloneNode(false);
+      cardLeft.classList.add("card");
+
+      const cardRight = div.cloneNode(false);
+      cardRight.classList.add("card");
+
+      const img = document.createElement("img");
+
+      const leftPin = img.cloneNode(false);
+      leftPin.setAttribute("src", "../img/alfinete.png");
+
+      const rightPin = img.cloneNode(false);
+      rightPin.setAttribute("src", "../img/alfinete.png");
+
+      const span = document.createElement("span");
+
+      const leftSpan = span.cloneNode(false);
+      leftSpan.innerHTML = `<str>Aplicativo para PetShop</str>`;
+
+      const rightSpan = span.cloneNode(false);
+      rightSpan.innerHTML = `Site para Pizzaria`;
+
+      const p = document.createElement("p");
+
+      const leftP = p.cloneNode(false);
+      leftP.innerHTML = `Possuo um Petshop e gostaria de um aplicativo para que meus clientes pudessem agendar serviços como: banho e tosa, taxi dog, hotelzinho e etc.`;
+
+      const leftResult = span.cloneNode(false);
+      leftResult.innerHTML = `Proposta Á Combinar`;
+
+      const rightResult = span.cloneNode(false);
+      rightResult.innerHTML = `Proposta Á Combinar`;
+
+      const rightP = p.cloneNode(false);
+      rightP.innerHTML = `	Preciso de um site para divulgar o sabores das pizzas, o endereço, os telefones de contato, nossas redes sociais.`;
+
+      cardLeft.appendChild(leftSpan);
+      cardLeft.appendChild(leftP);
+      cardLeft.appendChild(leftPin);
+      cardLeft.appendChild(leftResult);
+      divService.appendChild(cardLeft);
+
+      cardRight.appendChild(rightSpan);
+      cardRight.appendChild(rightP);
+      cardRight.appendChild(rightPin);
+      cardRight.appendChild(rightResult);
+      divService.appendChild(cardRight);
+      section.appendChild(divService);
+    },
+  };
+
+  const footerElement = {
+    footer: () => {
+      const footer = document.createElement("footer");
+      document.body.appendChild(footer);
+      footer.classList.add("footer");
+    },
   };
 
   const structure = {
@@ -133,6 +317,14 @@
       span: mainElement.span(),
       bannerButtons: mainElement.bannerButtons(),
       bannerLink: mainElement.bannerLink(),
+      clientConection: mainElement.clientConection(),
+      clientContent: mainElement.clientContent(),
+      clientButton: mainElement.clientButton(),
+      devSection: mainElement.devSection(),
+      serviceSection: mainElement.serviceSection(),
+    },
+    footer: {
+      footer: footerElement.footer(),
     },
   };
 
